7b179de631f43e65592d7e9e0fb5ed74015c3d61
Matthew Smith <mateoelcule@gmail.com>
2019-02-24 10:15:13 -0800

    feat: Add dispatch function
    The dispatch function takes in an action, calls the reducer function which changes the state, and then calls the listeners.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo]);
-  }
-}
+//Library code
 
-function createStore () {
+function createStore (reducer) {
   //The store should have four parts
   //1. The state
   //2. Get the state
@@ -22,17 +18,37 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners = listeners.forEach((listener) => listener());
+  }
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch
   }
 }
 
-const store = createStore();
-store.subscribe(() => {
-  console.log('The new state is: ', store.getState());
-});
+//App code
+
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo]);
+  }
+}
+
+const store = createStore(todos);
 
 store.subscribe(() => {
-  console.log('The store changed.');
+  console.log('The new state is ', store.getState());
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
 });
